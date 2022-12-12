5df287b24faf8e09e38badc74690e56e279b4985
Zé Pedro Matos <revolty@gmail.com>
2022-12-12 23:41:49 +0000

    day 4 - part 2

--- 4/4.js
+++ 4/4.js
@@ -2,9 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const assignments = (0, fs_1.readFileSync)("input.txt", "utf-8").split(/\r?\n/).map(assignment => assignment.split(","));
-const checkIfAssignmentsAreContained = ([...assignments]) => {
+const checkIfAssignmentsAreFullyContained = ([...assignments]) => {
     const [firstAssignment, secondAssignment] = assignments;
-    return getAssignmentContainment([firstAssignment, secondAssignment]) || getAssignmentContainment([secondAssignment, firstAssignment]);
+    return getAssignmentFullContainment([firstAssignment, secondAssignment]) || getAssignmentFullContainment([secondAssignment, firstAssignment]);
+};
+const checkIfAssignmentsArePartiallyContained = ([...assignments]) => {
+    const [firstAssignment, secondAssignment] = assignments;
+    return getAssignmentPartialContainment([firstAssignment, secondAssignment]) || getAssignmentPartialContainment([secondAssignment, firstAssignment]);
 };
 const getStartingRangeId = (assignment) => {
     const startingId = Number(assignment.split("-")[0]);
@@ -14,10 +18,16 @@ const getLastRangeId = (assignment) => {
     const lastId = Number(assignment.split("-")[1]);
     return lastId;
 };
-const getAssignmentContainment = ([...assignments]) => {
+const getAssignmentFullContainment = ([...assignments]) => {
     const [firstAssignment, secondAssignment] = assignments;
     return getStartingRangeId(firstAssignment) <= getStartingRangeId(secondAssignment) && getLastRangeId(firstAssignment) >= getLastRangeId(secondAssignment);
 };
-const assignmentsFullyContained = assignments.filter(assignment => checkIfAssignmentsAreContained(assignment));
+const getAssignmentPartialContainment = ([...assignments]) => {
+    const [firstAssignment, secondAssignment] = assignments;
+    return getStartingRangeId(firstAssignment) <= getStartingRangeId(secondAssignment) && getLastRangeId(firstAssignment) >= getStartingRangeId(secondAssignment) || getStartingRangeId(firstAssignment) <= getLastRangeId(secondAssignment) && getLastRangeId(firstAssignment) >= getLastRangeId(secondAssignment);
+};
+const assignmentsFullyContained = assignments.filter(assignment => checkIfAssignmentsAreFullyContained(assignment));
 const numberOfAssignmentsFullyContained = assignmentsFullyContained.length;
-console.log(numberOfAssignmentsFullyContained);
+const assignmentsContained = assignments.filter(assignment => checkIfAssignmentsArePartiallyContained(assignment));
+const numberOfAssignmentsContained = assignmentsContained.length;
+console.log(numberOfAssignmentsContained);
--- 4/4.ts
+++ 4/4.ts
@@ -2,10 +2,15 @@ import { readFileSync } from "fs";
 
 const assignments = readFileSync("input.txt", "utf-8").split(/\r?\n/).map(assignment => assignment.split(","));
 
-const checkIfAssignmentsAreContained = ([...assignments]: string[]): boolean => {
+const checkIfAssignmentsAreFullyContained = ([...assignments]: string[]): boolean => {
     const [firstAssignment, secondAssignment] = assignments;
-    return getAssignmentContainment([firstAssignment, secondAssignment]) || getAssignmentContainment([secondAssignment, firstAssignment]);
+    return getAssignmentFullContainment([firstAssignment, secondAssignment]) || getAssignmentFullContainment([secondAssignment, firstAssignment]);
 }
+
+const checkIfAssignmentsArePartiallyContained = ([...assignments]: string[]): boolean => {
+    const [firstAssignment, secondAssignment] = assignments;
+    return getAssignmentPartialContainment([firstAssignment, secondAssignment]) || getAssignmentPartialContainment([secondAssignment, firstAssignment]);
+};
 
 const getStartingRangeId = (assignment: string): number => {
     const startingId = Number(assignment.split("-")[0]);
@@ -17,13 +22,22 @@ const getLastRangeId = (assignment: string): number => {
     return lastId;
 };
 
-const getAssignmentContainment = ([...assignments]: string[]): boolean => {
+const getAssignmentFullContainment = ([...assignments]: string[]): boolean => {
     const [firstAssignment, secondAssignment] = assignments;
     return getStartingRangeId(firstAssignment) <= getStartingRangeId(secondAssignment) && getLastRangeId(firstAssignment) >= getLastRangeId(secondAssignment);
 };
 
-const assignmentsFullyContained = assignments.filter(assignment => checkIfAssignmentsAreContained(assignment));
+const getAssignmentPartialContainment = ([...assignments]: string[]): boolean => {
+    const [firstAssignment, secondAssignment] = assignments;
+    return getStartingRangeId(firstAssignment) <= getStartingRangeId(secondAssignment) && getLastRangeId(firstAssignment) >= getStartingRangeId(secondAssignment) || getStartingRangeId(firstAssignment) <= getLastRangeId(secondAssignment) && getLastRangeId(firstAssignment) >= getLastRangeId(secondAssignment);
+};
+
+const assignmentsFullyContained = assignments.filter(assignment => checkIfAssignmentsAreFullyContained(assignment));
 
 const numberOfAssignmentsFullyContained = assignmentsFullyContained.length;
 
-console.log(numberOfAssignmentsFullyContained);
+const assignmentsContained = assignments.filter(assignment => checkIfAssignmentsArePartiallyContained(assignment));
+
+const numberOfAssignmentsContained = assignmentsContained.length;
+
+console.log(numberOfAssignmentsContained);
