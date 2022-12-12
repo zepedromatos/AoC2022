b0a312e0c9faf6f12dbd76d41ae05ef40fd1c4d1
Zé Pedro Matos <revolty@gmail.com>
2022-12-12 22:50:32 +0000

    day 3 - part 2

--- 3/3.js
+++ 3/3.js
@@ -23,4 +23,23 @@ const getItemPriority = (item) => {
 };
 const ruckSackPriorities = commonItems.map(item => getItemPriority(item));
 const totalPriorities = ruckSackPriorities.reduce((a, b) => a + b, 0);
-console.log(totalPriorities);
+// part 2
+const getGroupsRacksacks = (ruckSacks) => {
+    let groupedRuckSacks = [];
+    for (let index = 0; index < ruckSacks.length; index += 3) {
+        groupedRuckSacks = [...groupedRuckSacks, [ruckSacks[index], ruckSacks[index + 1], ruckSacks[index + 2]]];
+    }
+    return groupedRuckSacks;
+};
+const groupedRuckSacks = getGroupsRacksacks(ruckSacks);
+const getCommonItemFromGroupRucksack = (ruckSackCompartment1, ruckSackCompartment2, ruckSackCompartment3) => {
+    const ruckSackCompartment1Array = ruckSackCompartment1.split("");
+    const ruckSackCompartment2Array = ruckSackCompartment2.split("");
+    const ruckSackCompartment3Array = ruckSackCompartment3.split("");
+    const commonItem = ruckSackCompartment1Array.filter((letter) => ruckSackCompartment2Array.includes(letter) && ruckSackCompartment3Array.includes(letter));
+    return commonItem[0];
+};
+const commonItemsFromGroupedRuckSacks = groupedRuckSacks.map(ruckSack => getCommonItemFromGroupRucksack(ruckSack[0], ruckSack[1], ruckSack[2]));
+const ruckSackPrioritiesFromGroupedRuckSacks = commonItemsFromGroupedRuckSacks.map(item => getItemPriority(item));
+const totalPrioritiesFromGroupedRuckSacks = ruckSackPrioritiesFromGroupedRuckSacks.reduce((a, b) => a + b, 0);
+console.log(totalPrioritiesFromGroupedRuckSacks);
